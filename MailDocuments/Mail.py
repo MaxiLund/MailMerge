950b00a674f9df4bf87c1ee25fffdfe93b05f8f7
MaxiLund <maxi.lund@outlook.com>
2022-07-30 21:55:11 +0000

    Implemented Docx Logic

--- MailDocuments/Mail.py
+++ MailDocuments/Mail.py
@@ -1,9 +1,20 @@
 from generic_app.models import *
 
 from generic_app.submodels.MailMerge.UploadFiles.MailMerge import MailMerge
+from mailmerge import MailMerge as Merge
+
 class Mail(Model):
     
-    mail_merge = ForeignKey()
-    docx_document = FileField()
-    pdf_document = FileField()
+    mail_merge = ForeignKey(to=MailMerge, on_delete=CASCADE)
+    file_name = TextField(default='')
+    docx_document = FileField(upload_to="submodels/MailMerge/docx_document/")
+    pdf_document = FileField(upload_to="submodels/MailMerge/pdf_document/")
     
+    def create_documents(self, row):
+        with Merge(self.mail_merge.mailmerge_docx) as document:
+            document.merge(**row)
+
+            docx_location = os.path.abspath(f'{settings.MEDIA_ROOT}/{self.mail_merge.file_path()}/docx_document/{self.file_name}.docx')
+            os.makedirs(os.path.dirname(docx_location), exist_ok=True)
+            document.write(docx_location)
+            self.docx_document.name=f"/{self.mail_merge.file_path()}/docx_document/{self.file_name}.docx"
--- UploadFiles/MailMerge.py
+++ UploadFiles/MailMerge.py
@@ -1,12 +1,43 @@
 from generic_app.models import *
+from ProcessAdminRestApi.models.upload_model import UploadModelMixin, ConditionalUpdateMixin
+from mailmerge import MailMerge as Merge
+import pandas as pd
 
-class MailMerge(UploadModelMixin, Model):
+class MailMerge(ConditionalUpdateMixin, UploadModelMixin, Model):
     
-    mailmerge_docx = FileField()
-    upload_template = FileField()
-    upload_data = FileField()
-    zip_docx = FileField()
+    name = TextField(default='')
+    mailmerge_docx = FileField(upload_to="submodels/MailMerge/mailmerge_docx/")
+    upload_template = XLSXField(upload_to="submodels/MailMerge/upload_template/")
+    upload_data = FileField(upload_to="submodels/MailMerge/upload_data/")
+    zip_docx = FileField(upload_to="submodels/MailMerge/zip_docx/")
     
+    def file_path(self):
+        return f"submodels/MailMerge/{self.name}"
+
+    def create_template(self):
+        with Merge(self.mailmerge_docx) as document:
+            merge_fields = ['document_name'] + list(document.get_merge_fields())
+            df = pd.DataFrame(columns=merge_fields)
+            XLSXField.create_excel_file_from_dfs(self.upload_template, path= "template.xlsx", data_frames=[df])
+            
+    def create_mails(self):
+        from generic_app.submodels.MailMerge.MailDocuments.Mail import Mail
+        columns = pd.read_excel(self.upload_data).columns
+        df = pd.read_excel(self.upload_data, converters={c:str for c in columns})
+        for index, row in df.iterrows():
+            mail = Mail(mail_merge=self, file_name=row['document_name'])
+            mail.save()
+            mail.create_documents(row)
+            mail.save()
+
+        # create zip
+        result = shutil.make_archive(MEDIA_ROOT + os.sep + self.file_path(), 'zip', MEDIA_ROOT + os.sep + self.file_path())
+        self.zip_file.name = self.file_path() + ".zip"
+
+
+    @ConditionalUpdateMixin.conditional_calculation
     def update(self):
-        # TODO specify what you want to do once the model has been saved
-        pass
+        
+        self.create_template()
+        self.create_mails()
+        
